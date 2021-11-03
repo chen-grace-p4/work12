61651bf874a6cbc92ea82081bd4072603d900329
Grace Chen <73039310+chen-grace-p4@users.noreply.github.com>
2021-11-03 04:44:16 +0000

    updated format

--- work12.c
+++ work12.c
@@ -18,15 +18,15 @@ int main() {
 
 	dent = readdir(d);
 
-	printf("Statistics for current directory . \n");
+	printf("Statistics for current directory: . \n");
 	while(dent) {
 		stat(dent->d_name, &sb);
 		if (dent->d_type == 8) {
 			size += sb.st_size;
-			printf("Regular File: %s \n", dent->d_name);
+			printf("\tRegular File: %s \n", dent->d_name);
 		}
 		if (dent->d_type == 4) {
-			printf("Directory: %s \n", dent->d_name);
+			printf("\tDirectory: %s \n", dent->d_name);
 		}
 		dent = readdir(d);
 	}
