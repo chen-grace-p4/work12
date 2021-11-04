a86b7e41f628a512dd19bd310c8a5c7777b48d5c
Grace Chen <73039310+chen-grace-p4@users.noreply.github.com>
2021-11-04 03:22:02 +0000

    added new

--- work12.c
+++ work12.c
@@ -8,54 +8,69 @@
 #include <time.h>
 #include <dirent.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+	char path[100];
+	if (argc > 1) {
+		strcp(path, argv[1]);
+	} else {
+		printf("Please enter a directory: ");
+		fgets(path, sizeof(path)-1, stdin);
+		path[strlen(path)-1] = 0;
+		printf("\n");
+	}
 	struct stat sb;
 
-	DIR *d = opendir(".");
+	DIR *d = opendir(path);
 	struct dirent *dent;
 
 	int size = 0;
 
 	dent = readdir(d);
 
-	printf("Statistics for current directory: . \n");
-	while(dent) {
-		stat(dent->d_name, &sb);
-		if (dent->d_type == 8) {
-			size += sb.st_size;
-			printf("\tRegular File: %s \n", dent->d_name);
-		}
-		if (dent->d_type == 4) {
-			printf("\tDirectory: %s \n", dent->d_name);
-		}
-		dent = readdir(d);
+	if(directory == NULL) {
+		printf("Error: %s\n", strerror(errno));
 	}
-
-	printf("Total Directory Size: %d Bytes\n", size);
-
-	printf("\nClearer print of files/directories:\n");
-
-
-	printf("Directories:\n");
-	rewinddir(d);
-	dent = readdir(d);
-	while (dent) {
-		if (dent->d_type == 4) {
-		printf("\t%s\n", dent->d_name);
+	if (directory) {
+		printf("Statistics for current directory: . \n");
+		while(dent) {
+			stat(dent->d_name, &sb);
+			if (dent->d_type == 8) {
+				size += sb.st_size;
+				printf("\tRegular File: %s \n", dent->d_name);
+			}
+			if (dent->d_type == 4) {
+				printf("\tDirectory: %s \n", dent->d_name);
+			}
+			dent = readdir(d);
 		}
-		dent = readdir(d);
-	}
 
-	printf("Regular files:\n");
-	rewinddir(d);
-	dent = readdir(d);
-	while (dent) {
-		if (dent->d_type == 8) {
-		printf("\t%s\n", dent->d_name);
+		printf("Total Directory Size: %d Bytes\n", size);
+
+		printf("\nClearer print of files/directories:\n");
+
+
+		printf("Directories:\n");
+		rewinddir(d);
+		dent = readdir(d);
+		while (dent) {
+			if (dent->d_type == 4) {
+			printf("\t%s\n", dent->d_name);
+			}
+			dent = readdir(d);
 		}
-		dent = readdir(d);
-	}
 
-	closedir(d);
+		printf("Regular files:\n");
+		rewinddir(d);
+		dent = readdir(d);
+		while (dent) {
+			if (dent->d_type == 8) {
+			printf("\t%s\n", dent->d_name);
+			}
+			dent = readdir(d);
+		}
+
+		closedir(d);
+	}
+	
 	return 0;
 }
